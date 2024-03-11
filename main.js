ed99ecaa3cf3a16650b352e9fe5e89de6dba97ba
Hadija <hadiaskarbek@gmail.com>
2024-03-11 10:01:10 +0600

    army

---
+++ main.js
@@ -0,0 +1,115 @@
+// ! Переменные 
+
+// ! Команды для создания переменных
+// ? var - имеет глобальную область видимости, не используется сейчас но 80% вебсайтов сделано при помощи var. Имеет проблему с хойстингом (hoising)
+// ? const - неизменяемые переменные 
+// ? let - изменяемое переменное
+
+// let studentName = "Hadija";
+// console.log(studentName);
+// studentName = "Kim";
+// console.log(studentName);
+
+// const studentAge = 16;
+// console.log(studentAge);
+// studentAge = 27;
+// console.log(studentAge);
+
+// ! Типы данных 
+
+// ! number     Число      let salary = 1000
+// ! string     Текст      let modal = "Passat"
+// ! boolean    Условность     let isHandsome = false
+// ! null       Специальная пустота  let coupleName = null
+// ! symbol     Идентификатор   let plane = Symbol('34')
+// ! bigint     Большое число   let promise;
+// ! underline  Пустота         let spaceNumber = e123456789076
+// ! obgect     Объект со свойствами      let student = {name: "Bahonty", age: 75,}
+                                                    
+
+// let age = 119;
+// let name = "Stalin";
+// let isDie = true;
+
+// let person = {
+//     name: "Pushkin",
+//     age: 97,
+//     isDie: true, 
+//     isHandsome:  false,
+// };
+
+// console.log(age);
+// console.log(name);
+// console.log(isDie);
+// console.log(person);
+
+// ! {} - фигурные скобки. Для объектов, когда пишем js код
+// ! [] - квадратные скобки. Для массивов, индексов 
+// ! () - круглые скобки. Для вызова функций, для  аргументов
+// !
+
+
+
+
+// 1)
+let singer = "Taehuyng";
+let age = 27;
+console.log(name);
+console.log(age);
+
+// 2)
+num1 = "5",
+num2 = "10",
+console.log(num1 + num2);
+
+// 3)
+let myFavoriteBook = "В метре друг от друга"
+let pagesInBook = 230;
+console.log(myFavoriteBook);
+console.log(pagesInBook);
+
+// 4)
+let student = {
+    name: "Yoongi",
+    age: 30,
+};
+console.log(name);
+console.log(age);
+console.log(student);
+
+
+// 5)
+let me = null;
+let our;
+console.log(me);
+console.log(our);
+
+
+// 6)
+let modal = "Tayota";
+let number = 712;
+let isLong = true;
+console.log(modal);
+console.log(number);
+console.log(isLong);
+
+let danser = {
+    name: "JHope",
+    age: 29,
+    isGoodDanser: true,
+};
+console.log(name);
+console.log(age);
+console.log(danser);
+
+// 7)
+let classList = 29; 
+// let потому что количество учащихся не постоянна 
+const myBirthDay = 230607;
+// const потому что мое день рождение никогда не поменяется 
+let temperatureAir = 21;
+// let потому что температура воздуха всегда меняется 
+console.log(classList);
+console.log(myBirthDay);
+console.log(temperatureAir);
+
